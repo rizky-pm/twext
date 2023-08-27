dba3957903dbd74b5c794792608b9b949c44f562
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2023-08-27 10:30:54 +0700

    Added edit profile page also the handlers for edit displayName, bio and photoURL

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,15 +1,134 @@
+import { useEffect, useState } from 'react';
+import { Stack, TextField } from '@mui/material';
 import { useForm } from 'react-hook-form';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+
+import { firebaseAuth, firestore } from '../utils/firebase';
+import { onAuthStateChanged, updateProfile } from 'firebase/auth';
+
+type EditProfileValues = {
+  displayName: string;
+  bio: string;
+  photoURL: string;
+};
 
 const EditProfile = () => {
+  const [detail, setDetail] = useState<EditProfileValues>({
+    displayName: '',
+    bio: '',
+    photoURL: '',
+  });
+
+  const { register, formState, handleSubmit, reset } =
+    useForm<EditProfileValues>({
+      defaultValues: {
+        displayName: '',
+        bio: '',
+        photoURL: '',
+      },
+    });
+
+  const { isSubmitting, errors } = formState;
+
+  const handleEditProfile = async (data: EditProfileValues) => {
+    const { bio, displayName, photoURL } = data;
+    const user = firebaseAuth.currentUser;
+
+    if (!user) {
+      return;
+    }
+
+    const promises = [];
+
+    if (displayName && displayName !== user.displayName) {
+      promises.push(updateProfile(user, { displayName }));
+    }
+
+    if (photoURL && photoURL !== user.photoURL) {
+      promises.push(updateProfile(user, { photoURL }));
+    }
+
+    if (bio) {
+      const userDetailRef = doc(firestore, 'userDetail', user.uid);
+      promises.push(updateDoc(userDetailRef, { bio }));
+    }
+
+    if (promises.length > 0) {
+      await Promise.all(promises);
+
+      // Update state if necessary
+      setDetail((prevState) => ({
+        ...prevState,
+        displayName: displayName || prevState.displayName,
+        bio: bio || prevState.bio,
+      }));
+
+      reset();
+    }
+  };
+
+  const getUserDetail = async () => {
+    onAuthStateChanged(firebaseAuth, async (user) => {
+      if (user) {
+        const detailRef = doc(firestore, 'userDetail', user?.uid);
+        const detailSnap = await getDoc(detailRef);
+        const userDetail = detailSnap.data();
+
+        setDetail({
+          displayName: user.displayName || '',
+          bio: userDetail?.bio || '',
+          photoURL: user.photoURL || '',
+        });
+
+        // reset({
+        //   displayName: user.displayName || "",
+        //   bio: userDetail?.bio || "",
+        //   photoURL: user.photoURL || "",
+        // });
+      }
+    });
+  };
+
+  useEffect(() => {
+    getUserDetail();
+  }, []);
+
   return (
     <section className='border-2 rounded-md flex flex-col p-4'>
-      <h1>Edit Profile Page</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil quam
-        beatae illo fugit non cumque quaerat aliquid ipsa eveniet sapiente,
-        minima esse doloribus exercitationem repellendus! Quas libero aspernatur
-        quos inventore?
-      </p>
+      <h1 className='font-bold text-xl mb-2'>Edit Profile</h1>
+
+      <form onSubmit={handleSubmit(handleEditProfile)} noValidate>
+        <Stack spacing={2}>
+          <TextField
+            id='displayName'
+            placeholder={detail.displayName || 'Display Name'}
+            variant='outlined'
+            size='small'
+            type='text'
+            fullWidth
+            {...register('displayName')}
+          />
+
+          <TextField
+            id='filled-multiline-flexible'
+            placeholder={detail.bio || 'Write interesting bio'}
+            multiline
+            fullWidth
+            rows={4}
+            {...register('bio')}
+            error={!!errors.bio}
+            helperText={errors.bio?.message}
+          />
+
+          <button
+            disabled={isSubmitting}
+            className='primary-button'
+            type='submit'
+          >
+            Edit
+          </button>
+        </Stack>
+      </form>
     </section>
   );
 };
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,5 +1,17 @@
+import { onAuthStateChanged } from "firebase/auth";
+
+import { firebaseAuth } from "./firebase";
+
 export const getUserFromLocalStorage = () => {
-  const currentUserUid = localStorage.getItem('user');
+  onAuthStateChanged(firebaseAuth, (user) => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+      // return user;
+      // ...
+    }
+  });
+
+  const currentUserUid = localStorage.getItem("user");
 
   if (currentUserUid) {
     const parsed = JSON.parse(currentUserUid);
